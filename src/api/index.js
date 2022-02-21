a92f49f34f31c404c4b417bdb755ebf333237d28
bonificial <mboniface107@gmail.com>
2022-02-21 16:42:09 +0300

    Update the API Requests  File

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,4 @@
+/******* Export the common API requests available in the directory from a single point ********/
+
 export { BASE_URL } from "./requests";
-export { pushDeliveryUpdates } from "./requests";
+export { pushDeliveryUpdates } from "./requests"; //Push Deliveries
--- src/api/requests.js
+++ src/api/requests.js
@@ -1,3 +1,4 @@
+/******* Export the common API requests ********/
 import axios from "axios";
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
